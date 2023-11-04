300e947e1a65d142f45acbd47e062f73e6903c8e
mailonnuunes <100615338+mailonnuunes@users.noreply.github.com>
2023-11-04 12:27:33 -0300

    Subindo primeiros arquivos

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+
+
+const livros = [
+    {
+        id: 1,
+        titulo: "senhor dos aneis"
+    },
+    {
+        id: 2,
+        titulo: "o hobbit"
+    }
+]
+app.get("/", (req,res)=>{
+    res.status(200).send("Curso de node.JS");
+});
+app.get("/livros", (req,res)=>{
+    res.status(200).json(livros);
+});
+app.post("/livros", (req,res)=>{
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso!")
+    
+});
+
+export default app;
